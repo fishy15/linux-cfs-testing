1d7f55d175fd2acbdff19e5467ee78e808cffbf2
aprakas <aprakas@node.aprakas-215492.wisr-pg0.utah.cloudlab.us>
2024-09-04 08:06:08 +0000

    Detect current logmsg entry and skip if null

--- dumper.py
+++ dumper.py
@@ -1,13 +1,47 @@
 import gdb
 
+def exec_capture_output(cmd):
+    return gdb.execute(cmd, False, True)
+
+def get_logmsg_ptr(output):
+    # looks like: "$1 = (struct karan_logmsg *) 0x0 <fixed_percpu_data>"
+    return output.split()[5]
+
+def get_entry(rq):
+    position = f'{rq}->cfs.karan_logbuf.position'
+    msg_size = f'{rq}->cfs.karan_logbuf.msg_size'
+    output = exec_capture_output(f'p {rq}->cfs.karan_logbuf.msgs[{position} * {msg_size}]')
+    return get_logmsg_ptr(output)
+
 def handle_rebalance_domains():
     print('handling rebalance domains...')
+    ptr = get_entry('rq')
+    
+    if ptr == '0x0':
+        print('it\'s null...')
+        gdb.execute('c')
+        return
+    print(ptr)
 
 def handle_newidle_balance():
     print('handling newidle balance...')
+    ptr = get_entry('this_rq')
+    
+    if ptr == '0x0':
+        print('it\'s null...')
+        gdb.execute('c')
+        return
+    print(ptr)
 
 def handle_karan_newidle_balance_ret():
     print('handling karan_newidle_balance_ret...')
+    ptr = get_logmsg_ptr(exec_capture_output('p buf'))
+    
+    if ptr == '0x0':
+        print('it\'s null...')
+        gdb.execute('c')
+        return
+    print(ptr)
 
 def breakpoint_handler(event):
     if isinstance(event, gdb.BreakpointEvent):
@@ -31,5 +65,3 @@ gdb.execute('b newidle_balance:out')
 gdb.execute('b karan_newidle_balance_ret')
 
 gdb.execute('c')
-
-
