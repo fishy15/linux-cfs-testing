3a7fbfa0a283e112aa51a3ce0dd2e94e8ecbc9a9
aprakas <aprakas@node.aprakas-215492.wisr-pg0.utah.cloudlab.us>
2024-09-22 04:45:58 +0000

    Update dumper script to print correct entries per iteration

--- dumper.py
+++ dumper.py
@@ -1,45 +1,72 @@
 import gdb
 
+print('TOPOLOGY:', TOPOLOGY)
+if not TOPOLOGY:
+    CORES=2
+else:
+    hyphen = TOPOLOGY.find('-')
+    if hyphen != -1:
+        CORES=int(TOPOLOGY[:hyphen])
+    else:
+        CORES=int(TOPOLOGY)
+
+print('CORES:', CORES)
+
+def exec(cmd):
+    print(cmd)
+    gdb.execute(cmd)
+
 def exec_capture_output(cmd):
+    print(cmd)
     return gdb.execute(cmd, False, True)
 
 def get_logmsg_ptr(output):
     # looks like: "$1 = (struct karan_logmsg *) 0x0 <fixed_percpu_data>"
+    print('OUTPUT:', output)
     return output.split()[5]
 
-def get_entry(rq):
+def get_slot(rq):
     position = f'{rq}->cfs.karan_logbuf.position'
-    msg_size = f'{rq}->cfs.karan_logbuf.msg_size'
-    output = exec_capture_output(f'p {rq}->cfs.karan_logbuf.msgs[{position} * {msg_size}]')
+    print('RQ:', exec_capture_output(f'p {rq}'), end='')
+    print('POSITION:', exec_capture_output(f'p {position}'), end='')
+    output = exec_capture_output(f'p {rq}->cfs.karan_logbuf.msgs[{position}]')
     return get_logmsg_ptr(output)
+
+ready_count = 0
+def handle_ready():
+    global ready_count
+    ready_count += 1
 
 def handle_rebalance_domains():
     print('handling rebalance domains...')
-    ptr = get_entry('rq')
+    ptr = get_slot('rq')
     
     if ptr == '0x0':
         print('it\'s null...')
-        gdb.execute('c')
+        exec('c')
         return
     print(ptr)
 
 def handle_newidle_balance():
     print('handling newidle balance...')
-    ptr = get_entry('this_rq')
+    ptr = get_slot('this_rq')
     
     if ptr == '0x0':
         print('it\'s null...')
-        gdb.execute('c')
+        exec('c')
         return
     print(ptr)
 
 def handle_karan_newidle_balance_ret():
+    # ignore for now
+    return
+
     print('handling karan_newidle_balance_ret...')
     ptr = get_logmsg_ptr(exec_capture_output('p buf'))
     
     if ptr == '0x0':
         print('it\'s null...')
-        gdb.execute('c')
+        exec('c')
         return
     print(ptr)
 
@@ -47,21 +74,38 @@ def breakpoint_handler(event):
     if isinstance(event, gdb.BreakpointEvent):
         bp = event.breakpoints[0]
         typ = bp.number
+
         if typ == 1:
-            handle_rebalance_domains()
+            handle_ready()
         elif typ == 2:
+            handle_rebalance_domains()
+        elif typ == 3:
             handle_newidle_balance()
-        else:
+        elif typ == 4:
             handle_karan_newidle_balance_ret()
+        else:
+            print('oh no')
 
 gdb.events.stop.connect(breakpoint_handler)
 
 # connect to remote
-gdb.execute('file /l/kbuild/vmlinux')
-gdb.execute('tar rem :1234')
+exec('file /l/kbuild/vmlinux')
+exec('tar rem :1234')
 
-gdb.execute('b rebalance_domains:out')
-gdb.execute('b newidle_balance:out')
-gdb.execute('b karan_newidle_balance_ret')
+exec('b karan_log_init:ready')
 
-gdb.execute('c')
+while ready_count < CORES:
+    exec('c')
+
+print('we are ready now')
+
+# once we have made everything ready
+exec('dis 1')
+exec('b rebalance_domains:out')
+exec('b newidle_balance:out')
+exec('b karan_newidle_balance_ret')
+
+print(exec_capture_output('show non-stop'))
+
+while True:
+    exec('c')
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8803,6 +8803,7 @@ struct karan_lb_logmsg {
  	struct lb_env env;
 };
 
+// values for a specific sched_domain iteration
 struct karan_rd_entry_logmsg {
 	u64 max_newidle_lb_cost;
 	int continue_balancing;
@@ -8821,6 +8822,7 @@ struct karan_rd_logmsg {
   	struct karan_rd_entry_logmsg *sd_buf;
 };
 
+// values for a specific sched_domain iteration
 struct karan_nb_entry_logmsg {
 	u64 curr_cost;
 	bool runs_load_balance;
@@ -11328,6 +11330,10 @@ static struct karan_logmsg *_karan_msg_alloc (struct rq *rq, enum karan_codepath
 	struct karan_logbuf *logbuf = &(rq->cfs.karan_logbuf);
 	if (logbuf->sd_count == 0) { return NULL; }
 
+	if (++logbuf->position == CONFIG_KARAN_LOGBUF_SIZE) {
+		logbuf->position = 0;
+	}
+
 	void *raw_msg_ptr = (logbuf->msg_area + logbuf->position * logbuf->msg_size);
 	struct karan_logmsg *msg = (struct karan_logmsg *) raw_msg_ptr;
 	logbuf->msgs[logbuf->position] = msg;
@@ -11337,10 +11343,6 @@ static struct karan_logmsg *_karan_msg_alloc (struct rq *rq, enum karan_codepath
 		msg->rd_msg.sd_buf = (struct karan_rd_entry_logmsg *) (raw_msg_ptr + sizeof(struct karan_logmsg));
 	} else {
 		msg->nb_msg.sd_buf = (struct karan_nb_entry_logmsg *) (raw_msg_ptr + sizeof(struct karan_logmsg));
-	}
-
-	if (++logbuf->position == CONFIG_KARAN_LOGBUF_SIZE) {
-		logbuf->position = 0;
 	}
 
 	return msg;
@@ -11355,12 +11357,15 @@ static inline struct karan_logmsg *karan_nb_msg_alloc (struct rq *rq) {
 }
 
 static void karan_log_init (struct rq *rq) {
+	LOG_TOPOLOGY("running karan log init\n");
+
 	struct karan_logbuf *logbuf = &(rq->cfs.karan_logbuf);
 	
 	int sd_count = 0;
 	struct sched_domain *sd;
 	for_each_domain(rq->cpu, sd) { sd_count++; }
-	if (sd_count == 0) { return; }
+	if (sd_count == 0) { return; } else { goto ready; }
+ready:
 	logbuf->sd_count = sd_count;
 	
 	int rd_msg_size = sd_count * sizeof(struct karan_rd_entry_logmsg);
@@ -11370,6 +11375,7 @@ static void karan_log_init (struct rq *rq) {
 	logbuf->msg_size = msg_size;
 	
 	logbuf->msg_area = kzalloc(msg_size * CONFIG_KARAN_LOGBUF_SIZE, GFP_KERNEL);
+	logbuf->position = CONFIG_KARAN_LOGBUF_SIZE - 1; // will be incremented on first alloc
 }
 
 /*
@@ -12496,7 +12502,7 @@ static int newidle_balance(struct rq *this_rq, struct rq_flags *rf)
 	}
 
 	struct karan_logmsg *msg = karan_nb_msg_alloc(this_rq);
-    struct karan_nb_logmsg *nb_msg = NULL;
+	struct karan_nb_logmsg *nb_msg = NULL;
 	if (msg != NULL) {
 		nb_msg = &msg->nb_msg;
 	}
