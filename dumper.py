9d93e1843ba553469f3f2c2872ecfbc94d3bfce1
aprakas <aprakas@node.aprakas-221027.wisr-pg0.utah.cloudlab.us>
2024-10-06 21:29:48 +0000

    Add further logging for lb_env

--- dumper.py
+++ dumper.py
@@ -2,7 +2,7 @@ import gdb
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import List
+from typing import List, Optional
 
 #### TYPES ####
 
@@ -16,13 +16,106 @@ class CpuIdleType(Enum):
     CPU_NEWLY_IDLE = 2
     CPU_MAX_IDLE_TYPES = 3
 
-# TODO: read it in
+def read_cpu_idle_type(idle):
+    idle_value = read_value(idle)
+    for option in CpuIdleType:
+        if option.name == idle_value:
+            return option
+    raise Exception(f'No matching idle type for {idle_value}')
+
 @dataclass
-class LBEnv:
+class SchedDomain:
     pass
 
+def read_sched_domain(sd):
+    print(sd)
+    return SchedDomain()
+
+@dataclass
+class ReadyQueue:
+    pass
+
+def read_ready_queue(rq):
+    print(read_value(rq))
+    return ReadyQueue()
+
+@dataclass
+class CpuMask:
+    pass
+
+def read_cpumask(cpumask):
+    print(cpumask)
+    return CpuMask()
+
+@dataclass
+class FBQType(Enum):
+    regular = 0
+    remote = 1
+    all = 2
+
+def read_fbq_type(fbq_type):
+    fbq_type_value = read_value(fbq_type)
+    for option in FBQType:
+        if option.name == fbq_type_value:
+            print('FBQ:', option)
+            return option
+    raise Exception(f'No matching fbq type for {fbq_type_value}')
+
+
+@dataclass
+class MigrationType(Enum):
+    migrate_load = 0
+    migrate_util = 1
+    migrate_task = 2
+    migrate_misfit = 3
+
+def read_migration_type(migration_type):
+    migration_type_value = read_value(migration_type)
+    for option in MigrationType:
+        if option.name == migration_type_value:
+            return option
+    raise Exception(f'No matching migration type for {migration_type_value}')
+
+
+@dataclass
+class LBEnv:
+    sd: Optional[SchedDomain]
+    src_rq: Optional[ReadyQueue]
+    src_cpu: int
+    dst_cpu: int
+    dst_rq: Optional[ReadyQueue]
+    dst_grpmask: CpuMask
+    new_dst_cpu: int
+    idle: CpuIdleType
+    imbalance: int
+    cpus: CpuMask
+    flags: int
+    loop: int
+    loop_break: int
+    loop_max: int
+    fbq_type: FBQType
+    migration_type: MigrationType
+    # tasks: ListHead
+
 def read_lb_env(lb_env) -> LBEnv:
-    return None
+    sd = read_if_not_null(f'{lb_env}.sd', read_sched_domain)
+    src_rq = read_if_not_null(f'{lb_env}.src_rq', read_ready_queue)
+    src_cpu = read_int(f'{lb_env}.src_cpu')
+    dst_cpu = read_int(f'{lb_env}.dst_cpu')
+    dst_rq = read_if_not_null(f'{lb_env}.dst_rq', read_ready_queue)
+    dst_grpmask = read_cpumask(f'(*{lb_env}.dst_grpmask)')
+    new_dst_cpu = read_int(f'{lb_env}.new_dst_cpu')
+    idle = read_cpu_idle_type(f'{lb_env}.idle')
+    imbalance = read_int(f'{lb_env}.imbalance')
+    cpus = read_cpumask(f'(*{lb_env}.cpus)')
+    flags = read_int(f'{lb_env}.flags')
+    loop = read_int(f'{lb_env}.loop')
+    loop_break = read_int(f'{lb_env}.loop_break')
+    loop_max = read_int(f'{lb_env}.loop_max')
+    fbq_type = read_fbq_type(f'{lb_env}.fbq_type')
+    migration_type = read_migration_type(f'{lb_env}.migration_type')
+    return LBEnv(sd, src_rq, src_cpu, dst_cpu, dst_rq, dst_grpmask, new_dst_cpu, idle,
+            imbalance, cpus, flags, loop, loop_break, loop_max, fbq_type, migration_type)
 
 
 @dataclass
@@ -30,7 +123,7 @@ class LBLogMsg:
     lb_env: LBEnv
 
 def read_lb_logmsg(lb_logmsg) -> LBLogMsg:
-    lb_env = read_lb_env(f'{lb_logmsg}.lb_env')
+    lb_env = read_lb_env(f'{lb_logmsg}.env')
     return LBLogMsg(lb_env)
 
 
@@ -46,14 +139,14 @@ class RDEntryLogMsg:
     new_busy: int
 
 def read_rebalance_domains_entry(entry) -> RDEntryLogMsg:
-    max_newidle_lb_cost = int(exec_capture_output(f'p {entry}.max_newidle_lb_cost', 2))
-    continue_balancing = int(exec_capture_output(f'p {entry}.continue_balancing', 2))
-    interval = int(exec_capture_output(f'p {entry}.interval', 2))
-    need_serialize = int(exec_capture_output(f'p {entry}.need_serialize', 2))
-    runs_load_balance = bool(exec_capture_output(f'p {entry}.interval', 2))
+    max_newidle_lb_cost = read_int(f'{entry}.max_newidle_lb_cost')
+    continue_balancing = read_int(f'{entry}.continue_balancing')
+    interval = read_int(f'{entry}.interval')
+    need_serialize = read_int(f'{entry}.need_serialize')
+    runs_load_balance = bool(read_value(f'{entry}.interval'))
     lb_logmsg = read_lb_logmsg(f'{entry}.lb_logmsg')
-    new_idle = exec_capture_output(f'p {entry}.new_idle', 2)
-    new_busy = int(exec_capture_output(f'p {entry}.new_busy', 2))
+    new_idle = read_cpu_idle_type(f'{entry}.new_idle')
+    new_busy = read_int(f'{entry}.new_busy')
     return RDEntryLogMsg(max_newidle_lb_cost, continue_balancing, interval, need_serialize, runs_load_balance,
             lb_logmsg, new_idle, new_busy)
 
@@ -66,10 +159,10 @@ class RDLogMsg:
     sd_buf: List[RDEntryLogMsg]
 
 def read_rebalance_domains(rd_msg) -> RDLogMsg:
-    cpu = int(exec_capture_output(f'p {rd_msg}.cpu', 2))
-    idle = exec_capture_output(f'p {rd_msg}.idle', 2)
-    sched_idle_cpu = int(exec_capture_output(f'p {rd_msg}.sched_idle_cpu', 2))
-    sd_buf = [read_rebalance_domains_entry(f'{rd_msg}.sd_buf[0]')] # TODO: read more than one entry
+    cpu = read_int(f'{rd_msg}.cpu')
+    idle = read_cpu_idle_type(f'{rd_msg}.idle')
+    sched_idle_cpu = read_int(f'{rd_msg}.sched_idle_cpu')
+    sd_buf = [read_rebalance_domains_entry(f'{rd_msg}.sd_buf[0]')]  # TODO: read more than one entry
     return RDLogMsg(cpu, idle, sched_idle_cpu, sd_buf)
 
 
@@ -95,39 +188,48 @@ def exec(cmd):
     print(cmd)
     gdb.execute(cmd)
 
-def exec_capture_output(cmd, arg=None):
+def exec_capture_output(cmd):
     print(cmd)
     output = gdb.execute(cmd, False, True).strip()
-    if arg is None:
-        return output
-    else:
-        return output.split()[arg]
+    return output
 
 ## helper functions to extract information out 
 
-def get_logmsg_ptr(output):
-    # looks like: "$1 = (struct karan_logmsg *) 0x0 <fixed_percpu_data>"
-    return output.split()[5]
-
 def get_slot(rq):
     position = f'{rq}->cfs.karan_logbuf.position'
-    print('RQ:', exec_capture_output(f'p {rq}'))
-    print('POSITION:', exec_capture_output(f'p {position}'))
-    output = exec_capture_output(f'p {rq}->cfs.karan_logbuf.msgs[{position}]')
-    return get_logmsg_ptr(output)
-
-def get_codepath(ptr):
-    codepath = exec_capture_output(f'p {ptr}->codepath')
-    return codepath.split()[2]
+    print('RQ:', read_value(rq))
+    print('POSITION:', read_value(position))
+    return read_value(f'{rq}->cfs.karan_logbuf.msgs[{position}]')
 
 def get_logmsg_info(ptr):
-    ptr = f'((struct karan_logmsg *) {ptr})'
-    codepath = get_codepath(ptr)
+    ptr = f'((struct karan_logmsg *) {ptr})'  # cast to the right type
+    codepath = read_value(f'{ptr}->codepath')
+    print('CODEPATH:', codepath)
     if codepath == 'REBALANCE_DOMAINS':
-        data = read_rebalance_domains(f'{ptr}.rd_msg')
+        data = read_rebalance_domains(f'{ptr}->rd_msg')
         print(data)
     else:
         pass
+
+def read_value(val):
+    tokens = exec_capture_output(f'p {val}').split()
+    # if the type is given of a pointer, then return the value afterwards
+    try:
+        ptr_token = tokens.index('*)')
+        return tokens[ptr_token+1]
+    except ValueError:
+        return tokens[2]
+
+def read_int(val):
+    return int(read_value(val))
+
+
+def read_if_not_null(ptr, f):
+    ptr_address = exec_capture_output(f'p {ptr}')
+    if '0x0' not in ptr_address.split():
+        return f(f'(*{ptr})')
+    else:
+        return None
 
 ## check that all cores have been readied
 
@@ -162,7 +264,7 @@ def handle_newidle_balance():
 
 def handle_karan_newidle_balance_ret():
     print('handling karan_newidle_balance_ret...')
-    ptr = get_logmsg_ptr(exec_capture_output('p buf'))
+    ptr = read_value('buf')
     
     if ptr == '0x0':
         print('it\'s null...')
