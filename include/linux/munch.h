f030081526c83ebc395d329080476382f9ee9350
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2025-02-14 07:51:19 +0000

    Add enum for deciding where to write

--- include/linux/munch.h
+++ include/linux/munch.h
@@ -2,10 +2,15 @@
 #define _LINUX_MUNCH_H
 
 struct munch_ops {
-    void (*munch64) (uint64_t);
+	void (*munch64) (uint64_t);
 };
 
 void munch64 (uint64_t);
 void set_muncher (struct munch_ops *);
+
+// location enum
+enum munch_location {
+	MUNCH_CPU_NUMBER,
+};
 
 #endif
--- kernel/sched/munch.rs
+++ kernel/sched/munch.rs
@@ -28,10 +28,12 @@ struct RustMunch {
 
 impl kernel::Module for RustMunch {
     fn init(_module: &'static ThisModule) -> Result<Self> {
-        pr_info!("rust munch says hi\n");
-
         const TABLE: kernel::bindings::munch_ops = *MunchOpsVTable::<RustMunch>::build();
         let mut ret = Self{table: TABLE};
+
+        let mut buf = RingBuffer::<LoadBalanceInfo>::new(3);
+        buf.move_next();
+        let _ = buf.current();
         
         // SAFETY: meowww
         unsafe {
@@ -70,16 +72,48 @@ trait Reset {
     fn new() -> Self;
 }
 
+#[derive(Clone)]
+struct LoadBalanceInfo {}
+
+impl Reset for LoadBalanceInfo {
+    fn reset(&mut self) {}
+    fn new() -> Self {
+        LoadBalanceInfo {}
+    }
+}
+
 struct RingBuffer<T: Reset> {
     entries: KVec<T>,
     head: usize,
 }
 
+// locations
+
+/*
+fn open_meal() -> MealDescriptor {
+    return index;
+}
+
+fn munch(md: MealDescriptor, location: Location, value: u64) {
+    let entry = &mut ring_buffer[md];
+    match location {
+        CPU_NUMBER => entry.cpu_number = value,
+        CPU_LOAD => entry.cpu_load = value,
+    }
+}
+
+fn munch_index(md: MealDescriptor, location: Location, index: usize, value: 64) {
+    match location {
+        GROUP_LOAD => entry.groups[index].group_load = value
+    }
+}
+*/
+
 impl<T: Reset + Clone> RingBuffer<T> {
     fn new(n: usize) -> Self {
         return RingBuffer {
             entries: kvec![T::new(); n].expect("allocation error"),
-            head: n - 1, // will be shifted to 0 on first move next
+            head: n - bindings::munch_location::MUNCH_CPU_NUMBER as usize, // will be shifted to 0 on first move next
         };
     }
 
