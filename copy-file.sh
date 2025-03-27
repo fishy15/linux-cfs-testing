5e21d9cef35d5e88763f44247c649171fd1f1c86
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2025-03-27 04:48:20 +0000

    Use extra disk for more space

--- copy-file.sh
+++ copy-file.sh
@@ -1,3 +1,4 @@
 CPU=$1
-ssh -p2222 root@localhost "cat /proc/munch/$CPU > ~/$CPU.txt"
-scp -P2222 root@localhost:"~/$CPU.txt" "$CPU.txt"
+ssh -p2222 root@localhost "mount /dev/sdb1 /mnt"
+ssh -p2222 root@localhost "cat /proc/munch/$CPU > /mnt/$CPU.txt"
+scp -P2222 root@localhost:"/mnt/$CPU.txt" "$CPU.txt"
